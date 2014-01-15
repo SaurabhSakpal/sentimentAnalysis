75bdd7ef35d1089dd58d81a1f860325156f4f8ee
Saurabh <ssscoder@ssscoderPC.(none)>
2014-01-16 01:12:50 +0530

    Saurabh Sakpal -commit 1.1

--- fileRename.py
+++ fileRename.py
@@ -1,18 +1,19 @@
 import os;
-
-dir_list =["neg","pos"];
+#dir_list =["neg","pos"];
+#file_list = os.listdir("neg");
+file_list = os.listdir("pos");
 num = 1; 
-for s in dir_list:
-	file_list = os.listdir(s);
-	for k in file_list:
-		newname = "neg/Rev"+str(num)+".txt"
-		#print(newname);
-		oldname = s+"/"+k;
-		#print(oldname); 
-		os.rename(oldname,newname);
-		num = num +1;
-	#print(len(file_list));
-file_list_copy = os.listdir("neg");
+for k in file_list:
+	newname = "pos/Rev"+str(num)+".txt"
+	#print(newname);
+	oldname = "pos/"+k;
+	#oldname = "neg/"+k;
+	#print(oldname); 
+	os.rename(oldname,newname);
+	num = num +1;
+#print(file_list.sort());
+
+
 
 
 
